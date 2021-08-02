8c22b5eae95923e967ae6385e0db3f3d2cdb0e7f
Ivan Kovalenko <s1ckretbox@gmail.com>
2021-08-02 16:04:13 +0300

    fix(PlayerWrapper): implement correct model changing
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

--- src/server/components/PlayerWrapper.ts
+++ src/server/components/PlayerWrapper.ts
@@ -1,24 +1,27 @@
-import { StarterPlayer } from "@rbxts/services";
 import SpaceShip from "./spaceship/SpaceShip";
 
 export default class PlayerWrapper {
     private readonly _player: Player;
     private _spaceship: SpaceShip;
     private _model: Model;
+    private _characterAddedConnection: RBXScriptConnection;
 
     constructor(player: Player) {
         this._player = player;
         this._spaceship = new SpaceShip();
-        this._model = this._CreateModel(this._spaceship);
+        this._model = this._CreateModel(this._player.Name, this._spaceship);
+        this._characterAddedConnection = player.CharacterAdded.Connect((model) => {
+            this._OnCharacterAdded(model);
+        });
     }
 
     public GetSpaceship(): SpaceShip {
         return this._spaceship;
     }
 
-    private _CreateModel(spaceship: SpaceShip): Model {
+    private _CreateModel(playerName: string, spaceship: SpaceShip): Model {
         const model: Model = new Instance("Model");
-        model.Name = "StarterCharacter";
+        model.Name = playerName;
 
         const primaryPart: Part = spaceship.GetCarcass().GetPart();
         primaryPart.Name = "HumanoidRootPart";
@@ -28,7 +31,18 @@ export default class PlayerWrapper {
         humanoid.Parent = model;
 
         model.PrimaryPart = primaryPart;
-        model.Parent = StarterPlayer;
         return model;
+    }
+
+    private _OnCharacterAdded(oldModel: Model): void {
+        // Disconnect to prevent recursion.
+        this._characterAddedConnection.Disconnect();
+
+        const oldCFrame: CFrame = oldModel.GetPrimaryPartCFrame();
+        this._model.SetPrimaryPartCFrame(oldCFrame);
+        oldModel.Destroy();
+
+        this._player.Character = this._model;
+        this._model.Parent = game.Workspace;
     }
 }
