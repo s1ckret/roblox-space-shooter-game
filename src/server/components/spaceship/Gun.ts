586e89dea23cc170a37bb21ee0e397967e8094e8
Ivan Kovalenko <s1ckretbox@gmail.com>
2021-08-02 22:04:43 +0300

    feat(Gun): add bullets on Shoot
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

--- src/server/components/spaceship/Gun.ts
+++ src/server/components/spaceship/Gun.ts
@@ -1,19 +1,44 @@
 import Insertable from "../slot/Insertable";
 import FastCastRedux from "server/vendor/FastCastRedux";
-
-FastCastRedux.VisualizeCasts = true;
+import { ActiveCast, FastCastBehavior } from "server/vendor/FastCastRedux/TypeDefinitions";
+import * as BulletFactory from "server/service/BulletFactory";
 
 export default class Gun implements Insertable {
     private readonly _part: Part;
     private _attachment: Attachment;
     private _attachmentFire: Attachment;
     private _caster: FastCastRedux;
+    private _castBehaviour: FastCastBehavior;
 
     constructor(part: Part) {
         this._part = part;
         this._attachment = <Attachment>this._part.WaitForChild("AttachmentGun");
         this._attachmentFire = <Attachment>this._part.WaitForChild("AttachmentFire");
         this._caster = new FastCastRedux();
+        this._caster.LengthChanged.Connect(
+            (
+                cast: ActiveCast,
+                lastPoint: Vector3,
+                rayDir: Vector3,
+                rayDisplacement: number,
+                segmentVelocity: Vector3,
+                cosmeticBulletObject?: Instance,
+            ) => {
+                this._OnLenghChanged(cast, lastPoint, rayDir, rayDisplacement, segmentVelocity, cosmeticBulletObject);
+            },
+        );
+        this._caster.RayHit.Connect(
+            (
+                cast: ActiveCast,
+                resultOfCast: RaycastResult,
+                segmentVelocity: Vector3,
+                cosmeticBulletObject?: Instance,
+            ) => {
+                this._OnRayHit(cast, resultOfCast, segmentVelocity, cosmeticBulletObject);
+            },
+        );
+        this._castBehaviour = FastCastRedux.newBehavior();
+        this._ConfigCastBehaviour(this._castBehaviour, [this._part, BulletFactory.GetFolderForTempBullets()]);
     }
 
     public Destroy(): void {
@@ -31,6 +56,54 @@ export default class Gun implements Insertable {
     public Shoot(): void {
         const origin: Vector3 = this._attachmentFire.WorldPosition;
         const direction: Vector3 = this._part.CFrame.LookVector;
-        this._caster.Fire(origin, direction, 10);
+        this._caster.Fire(origin, direction, 250, this._castBehaviour);
+    }
+
+    private _ConfigCastBehaviour(behaviour: FastCastBehavior, transparentInstances: Instance[]): void {
+        const raycastParams = new RaycastParams();
+        raycastParams.IgnoreWater = true;
+        raycastParams.FilterType = Enum.RaycastFilterType.Blacklist;
+        raycastParams.FilterDescendantsInstances = transparentInstances;
+
+        // TODO: Read from properties
+        behaviour.RaycastParams = raycastParams;
+        behaviour.AutoIgnoreContainer = false;
+        behaviour.MaxDistance = 50;
+        behaviour.CosmeticBulletContainer = BulletFactory.GetFolderForTempBullets();
+        behaviour.CosmeticBulletTemplate = BulletFactory.GetBullet(BulletFactory.BulletType.Rocket).part;
+    }
+
+    private _OnLenghChanged(
+        cast: ActiveCast,
+        lastPoint: Vector3,
+        rayDir: Vector3,
+        rayDisplacement: number,
+        segmentVelocity: Vector3,
+        cosmeticBulletObject?: Instance,
+    ): void {
+        if (cosmeticBulletObject !== undefined) {
+            const bulletLength: number = (<Part>cosmeticBulletObject).Size.Z / 2.0;
+            const offset: CFrame = new CFrame(0, 0, -(rayDisplacement - bulletLength));
+            (<Part>cosmeticBulletObject).CFrame = CFrame.lookAt(lastPoint, lastPoint.add(rayDir)).ToWorldSpace(offset);
+        }
+    }
+
+    private _OnRayHit(
+        cast: ActiveCast,
+        resultOfCast: RaycastResult,
+        segmentVelocity: Vector3,
+        cosmeticBulletObject?: Instance,
+    ): void {
+        const hit: Instance = resultOfCast.Instance;
+
+        const chracter: Model | undefined = hit.FindFirstAncestorWhichIsA("Model");
+        if (chracter !== undefined) {
+            const humanoid: Humanoid | undefined = <Humanoid>chracter.FindFirstChild("Humanoid");
+            if (humanoid !== undefined) {
+                humanoid.TakeDamage(10);
+            }
+        }
+
+        cosmeticBulletObject?.Destroy();
     }
 }
